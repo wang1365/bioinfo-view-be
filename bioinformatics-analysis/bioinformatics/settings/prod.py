e3484aff0d913c17261ccf014b1c36c33549d0ad
wanhua-gu <guwanhua@turingvideo.net>
2022-08-14 16:52:17 +0800

    update docker-compose.yml

--- bioinformatics-analysis/bioinformatics/settings/prod.py
+++ bioinformatics-analysis/bioinformatics/settings/prod.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-
+import os
 
 from bioinformatics.settings import *  # noqa
 
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "bioinfo",
-        "USER": "postgres",
-        "PASSWORD": "123456",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
         "HOST": "db",
         "PORT": "5432",
     }
