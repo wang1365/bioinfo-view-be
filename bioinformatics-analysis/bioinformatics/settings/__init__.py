28ed546a58f30603e0c15ad317163f496e4c6e57
Mering.Gao <canhuayin@gmail.com>
2022-08-20 21:57:37 +0800

    增加 cors 配置

--- bioinformatics-analysis/bioinformatics/settings/__init__.py
+++ bioinformatics-analysis/bioinformatics/settings/__init__.py
@@ -15,48 +15,33 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = "lm44m5=bg*+f^2v^2&rfl)cs5e&5g+50@*p7!u-j&@fmg1yv-_"
 
-SECRET_KEY = (
-    os.environ.get("DJANGO_SECRET_KEY")
-    or "lm44m5=bg*+f^2v^2&rfl)cs5e&5g+50@*p7!u-j&@fmg1yv-_"
-)
+SECRET_KEY = (os.environ.get("DJANGO_SECRET_KEY")
+              or "lm44m5=bg*+f^2v^2&rfl)cs5e&5g+50@*p7!u-j&@fmg1yv-_")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-    "django.contrib.sites",
-    'django_extensions',
-    'rest_framework',
-    "account",
-    "project",
-    "sample",
-    "task",
-    "rbac",
-    "flow",
-    "config",
-    "appearance",
-    "patient"
+    "django.contrib.admin", "django.contrib.auth",
+    "django.contrib.contenttypes", "django.contrib.sessions",
+    "django.contrib.messages", "django.contrib.staticfiles",
+    "django.contrib.sites", 'django_extensions', 'corsheaders',
+    'rest_framework', "account", "project", "sample", "task", "rbac", "flow",
+    "config", "appearance", "patient"
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -88,7 +73,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "bioinformatics.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
@@ -104,13 +88,22 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        "NAME":
+        "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
-    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
+    {
+        "NAME":
+        "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME":
+        "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME":
+        "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
@@ -134,12 +127,10 @@ USE_L10N = True
 
 USE_TZ = False
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = "/static/"
-
 
 # email config
 # EMAIL_HOST_PASSWORD = "vefqvvmganwtebfe"
@@ -151,7 +142,6 @@ STATIC_URL = "/static/"
 
 MEDIA_URL = "/media/"
 
-
 EMAIL_SENDER = "reporting2@nanodigmbio.com"
 EMAIL_HOST = "smtp.qiye.163.com"
 EMAIL_PORT = 994
@@ -160,8 +150,8 @@ EMAIL_PASSWORD = "Nano2020"
 # AUTH_USER_MODEL = "account.Account"
 SITE_ID = 1
 APPEND_SLASH = False
-ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv(
-    "ACCESS_TOKEN_EXPIRE_MINUTES", 60 * 600)
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES",
+                                        60 * 600)
 
 MAX_TASK = os.getenv("MAX_TASK", 10)
 
@@ -191,7 +181,6 @@ SAMPLE_SHELL_ENV = {
     "backup10": "backup10",
 }
 
-
 BAM_PATH = os.getenv("BAM_PATH", "/nano/bam")
 
 TASK_RESULT_DIR = os.getenv("TASK_RESULT_DIR", "/nano/web")
@@ -200,14 +189,24 @@ MOVE_OTHERS_DIR = os.getenv("MOVE_OTHERS_DIR", "/nano/result/other")
 MOVE_QC_DIR = os.getenv("MOVE_QC_DIR", "/nano/result/qc")
 
 CACHES = {
-     "default": {
-         "BACKEND": "django_redis.cache.RedisCache",
-         "LOCATION": "redis://redis:6379/1", #这里直接使用redis别名作为host ip地址
-         "OPTIONS": {
-             "CLIENT_CLASS": "django_redis.client.DefaultClient",
-             # "PASSWORD": "yourpassword", # 换成你自己密码
-         },
-     }
- }
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/1",  #这里直接使用redis别名作为host ip地址
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            # "PASSWORD": "yourpassword", # 换成你自己密码
+        },
+    }
+}
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
