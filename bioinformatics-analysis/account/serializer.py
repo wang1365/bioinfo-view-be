68426668c59571b98139d0ca41ee7a62495f868d
wangxiaochuan <wangxiaochuan01@163.com>
2022-09-07 08:57:34 +0800

    add nickname in account serializer

--- bioinformatics-analysis/account/serializer.py
+++ bioinformatics-analysis/account/serializer.py
@@ -6,4 +6,4 @@ from account.models import Account
 class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = Account
-        fields = ["id", "username", "email", "is_active", "department", "password", "disk_limit", "used_disk"]
+        fields = ["id", "username", "email", "is_active", "department", "password", "nickname", "disk_limit", "used_disk"]
