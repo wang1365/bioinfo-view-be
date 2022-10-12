5c67671313b33dde357bd0d662203f825ae404d4
wangxiaochuan <wangxiaochuan01@163.com>
2022-10-12 08:46:33 +0800

    add log file support

--- bioinformatics-analysis/bioinformatics/settings/prod.py
+++ bioinformatics-analysis/bioinformatics/settings/prod.py
@@ -14,6 +14,8 @@ DATABASES = {
     }
 }
 
+log_dir = os.path.join(os.getenv('BIO_ROOT', 'logs/bioinformatics'))
+os.makedirs(log_dir, exist_ok=True)
 LOGLEVEL = os.getenv('DJANGO_LOGLEVEL', 'info').upper()
 LOGGING = {
     'version': 1,
@@ -37,11 +39,26 @@ LOGGING = {
             'class': 'logging.StreamHandler',
             'formatter': 'simple'
         },
+        'file': {
+            # 实际开发建议使用ERROR
+            'level': 'INFO',
+            'class': 'concurrent_log_handler.ConcurrentRotatingFileHandler',
+            # 日志位置,日志文件名,日志保存目录必须手动创建，注：这里的文件路径要注意BASE_DIR代表的是小love
+            'filename': os.path.join(log_dir, "bioinformatics.log"),
+            # 日志文件的最大值,这里我们设置300M
+            'maxBytes': 300 * 1024 * 1024,
+            # 日志文件的数量,设置最大日志数量为10
+            'backupCount': 10,
+            # 日志格式:详细格式
+            'formatter': 'verbose',
+            # 文件内容编码
+            'encoding': 'utf-8'
+        },
     },
     'loggers': {
         '': {
             'level': LOGLEVEL,
-            'handlers': ['console'],
+            'handlers': ['console', 'file'],
         },
         'django.db.backends': {
             'handlers': ['db'],
