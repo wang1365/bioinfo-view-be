e3030a8902ee27a736a596ec86c029747aa1e035
张荣 <hanzejl@hotmail.com>
2022-11-08 08:03:24 +0800

    更新报告启动错误

--- bioinformatics-analysis/report/urls.py
+++ bioinformatics-analysis/report/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     url(
-        r"^/report/(?P<taskid>\d+)/(?P<name>[a-zA-Z_]+)/metadata", views.get_meta_data
+        r"^/metadata/(?P<taskid>\d+)/(?P<name>[a-zA-Z_]+)/", views.get_meta_data
     ),
     url(
-        r"^/report/(?P<taskid>\d+)/(?P<name>[a-zA-Z_]+)/data", views.get_raw_data
+        r"^/data/(?P<taskid>\d+)/(?P<name>[a-zA-Z_]+)/", views.get_raw_data
     ),
 ]
--- bioinformatics-analysis/report/views.py
+++ bioinformatics-analysis/report/views.py
@@ -2,14 +2,14 @@ import os
 import json
 
 from task.models import Task
-from core import generate_df, extract_meta_data, extract_data
-from constant import FILE_MAPPINGS
+from report.core import generate_df, extract_meta_data, extract_data
+from report.constant import FILE_MAPPINGS
 
 
 def get_meta_data(request, taskid, name):
     config = FILE_MAPPINGS[name]
-    task = Task.models.get(id=taskid)
-    filename = os.path.join(task.result_path, config['filepath'])
+    task = Task.objects.get(id=taskid)
+    filename = os.path.join(task.result_dir, config['filepath'])
     columns = json.loads(request.body)
     df = generate_df(filename, sep=config['sep'], header=config['header'])
     return extract_meta_data(df, columns)
@@ -17,8 +17,8 @@ def get_meta_data(request, taskid, name):
 
 def get_raw_data(request, taskid, name):
     config = FILE_MAPPINGS[name]
-    task = Task.models.get(id=taskid)
-    filename = os.path.join(task.result_path, config['filepath'])
+    task = Task.objects.get(id=taskid)
+    filename = os.path.join(task.result_dir, config['filepath'])
     query = json.loads(request.body)
     df = generate_df(filename, sep=config['sep'], header=config['header'])
     return extract_data(df, query)
