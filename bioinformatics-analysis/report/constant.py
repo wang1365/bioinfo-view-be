217d9c2bbce55eac9e28e7cc9c1a10ea032600b6
wangxiaochuan <wangxiaochuan01@163.com>
2022-11-10 23:10:51 +0800

    add file mapping

--- bioinformatics-analysis/report/constant.py
+++ bioinformatics-analysis/report/constant.py
@@ -39,6 +39,7 @@ MUT_SOMATIC = {
 }
 
 FILE_MAPPINGS = {
+    # 质控
     "QC": QC,
     "QN11": QN11,
     "QT11": QT11,
@@ -46,4 +47,26 @@ FILE_MAPPINGS = {
     "MUT_SOMATIC": MUT_SOMATIC,
     "ONTARGET": ONTARGET,
     "QC_TIP": QC_TIP,
+
+    # 突变分析
+
+    # 融合分析
+    "FUSION_QT11": {
+        "filepath": "fusion_germline/QT11.fusions",
+        "sep": ",",
+        "header": 0
+    },
+    "FUSION_QN11": {
+        "filepath": "fusion_germline/QN11.fusions",
+        "sep": ",",
+        "header": 0
+    },
+
+    # 拷贝数变异分析
+
+    # 微卫星不稳定结果
+
+    # 肿瘤突变符合分析结果
+
+    # 同源重组缺陷分析结果
 }
