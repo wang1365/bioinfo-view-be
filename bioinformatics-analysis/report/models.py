95d63a7db36983d23cd355ab2444e07d8692a303
wangxiaochuan <wangxiaochuan01@163.com>
2022-12-27 21:48:02 +0800

    修改 model

--- bioinformatics-analysis/report/models.py
+++ bioinformatics-analysis/report/models.py
@@ -14,12 +14,12 @@ class Report(models.Model):
                              default=None,
                              on_delete=DO_NOTHING)
 
-    #    script = models.CharField(max_length=512)
+    #script = models.CharField(max_length=512,null=True)
 
     creator = models.ForeignKey(to=Account, on_delete=models.CASCADE)
 
     query = models.TextField()
     comment = models.TextField(null=True)
     report_path = models.TextField(null=True)
-    create_time = models.DateTimeField(default=now)
-    status = models.CharField()
+    create_time = models.DateTimeField(default=now,null=True)
+    status = models.CharField(max_length=32,null=True)
