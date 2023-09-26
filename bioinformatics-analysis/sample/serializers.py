8ae8c271af741e5d51a795128d01b1b6a66d7891
Mering <canhuayin@gmail.com>
2023-09-26 15:11:30 +0000

    允许空

--- bioinformatics-analysis/sample/serializers.py
+++ bioinformatics-analysis/sample/serializers.py
@@ -15,16 +15,27 @@ class SampleMetaSerializer(serializers.ModelSerializer):
     class Meta:
         model = SampleMeta
 
-        fields = ['id', 'patient', 'patient_id', 'identifier', 'user',
-                  'user_id'] + [f['key'] for f in SAMPLE_META_MODEL_ATTRS]
+        fields = ["id", "patient", "patient_id", "identifier", "user", "user_id"] + [
+            f["key"] for f in SAMPLE_META_MODEL_ATTRS
+        ]
 
 
 class SampleSerializer(serializers.ModelSerializer):
     sample_meta_id = serializers.IntegerField(allow_null=True, default=None)
     sample_meta = SampleMetaSerializer(read_only=True)
-    patient = PatientSerializer(source='sample_meta.patient', read_only=True)
+    patient = PatientSerializer(source="sample_meta.patient", read_only=True)
+    fastq2_path = serializers.CharField(
+        allow_null=True, required=False, default="", allow_blank=True
+    )
 
     class Meta:
         model = Sample
-        fields = ['id', 'sample_meta', 'patient', 'sample_meta_id',
-                  'user', 'user_id', 'identifier'] + [f['key'] for f in SAMPLE_MODEL_ATTRS]
+        fields = [
+            "id",
+            "sample_meta",
+            "patient",
+            "sample_meta_id",
+            "user",
+            "user_id",
+            "identifier",
+        ] + [f["key"] for f in SAMPLE_MODEL_ATTRS]
