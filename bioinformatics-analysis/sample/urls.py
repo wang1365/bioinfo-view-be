5124195355f6994eacc48be04fa1fefc9204a0d2
Mering <Mering>
2024-03-16 15:23:49 +0800

    样本导出

--- bioinformatics-analysis/sample/urls.py
+++ bioinformatics-analysis/sample/urls.py
@@ -8,16 +8,17 @@ router = routers.DefaultRouter()
 
 router.register(r'^/samples', views.SampleView, basename="samples")
 
-router.register(r'^/sampledatas', views.SampleMetaView, basename="samples_datas")
+router.register(r'^/sampledatas',
+                views.SampleMetaView,
+                basename="samples_datas")
 
 other_urls = [
     url(r"^/download/(?P<pk>\d+)$", views.download),
-
     url(
         r"^/samples/template/download",
-        lambda req: views.download_by_choices(req, [SAMPLE_TEMPLATE_PATH, SAMPLE_TEMPLATE_EN_PATH]),
+        lambda req: views.download_by_choices(
+            req, [SAMPLE_TEMPLATE_PATH, SAMPLE_TEMPLATE_EN_PATH]),
     ),
-
     url(
         r"^/samples/(?P<pk>\d+)/detail",
         views.SampleView.as_view({"get": "retrieve"}),
@@ -30,25 +31,23 @@ other_urls = [
         r"^/samples/export",
         views.SampleView.as_view({"get": "export"}),
     ),
-     url(
+    url(
         r"^/samplemeta/export",
-        views.SampleView.as_view({"get": "export"}),
+        views.SampleMetaView.as_view({"get": "export"}),
     ),
     url(r"^/samples/list_fields",
         views.SampleMetaView.as_view({"get": "list_fields"})),
-
     url(
         r"^/samplemeta/template/download",
-        lambda req: views.download_by_choices(req, [SAMPLE_META_TEMPLATE_PATH, SAMPLE_META_TEMPLATE_EN_PATH]),
+        lambda req: views.download_by_choices(
+            req, [SAMPLE_META_TEMPLATE_PATH, SAMPLE_META_TEMPLATE_EN_PATH]),
     ),
     url(r"^/samplemeta/list_fields",
         views.SampleMetaView.as_view({"get": "list_fields"})),
-
     url(r"^/samplemeta/upload",
         views.SampleUploadView.as_view({"post": "upload_meta"})),
-
-    url(r"^/samples/upload",
-        views.SampleUploadView.as_view({"post": "upload"})),
+    url(r"^/samples/upload", views.SampleUploadView.as_view({"post":
+                                                             "upload"})),
 ]
 
 urlpatterns = other_urls + router.urls
