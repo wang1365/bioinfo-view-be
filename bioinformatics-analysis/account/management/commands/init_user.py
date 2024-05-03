b75523dd57e3c124b95dfe52095c9a23f9782279
Wang, Xiaochuan <wangxiaochuan01@163.com>
2024-05-03 19:48:10 +0800

    error fix

--- bioinformatics-analysis/account/management/commands/init_user.py
+++ bioinformatics-analysis/account/management/commands/init_user.py
@@ -8,24 +8,24 @@ from utils.site import get_md5
 class Command(BaseCommand):
     help = 'init user account'
 
+    def create_role(self, code):
+        if not Role.objects.filter(code=code).exists():
+            Role.objects.create(Role(code=code))
+
     def handle(self, *args, **options):
-        Role.objects.all().delete()
-        Role.objects.bulk_create([
-            Role(code="super"),
-            Role(code="admin"),
-            Role(code="normal"),
-        ])
+        self.create_role("super")
+        self.create_role("admin")
+        self.create_role("normal")
+
         super_role = Role.objects.get(code="super")
         admin_role = Role.objects.get(code="admin")
 
-        if not Account.objects.filter(email="super@super.com").exists():
-            super_account, _ = Account.objects.update_or_create(
-                defaults={'username': "super", 'nickname': '超级管理员', 'password': get_md5("1234qwer"), 'is_active': True},
-                email="super@super.com")
-            User2Role.objects.update_or_create(user=super_account, role=super_role)
+        super_account, _ = Account.objects.update_or_create(
+            defaults={'username': "super", 'nickname': '超级管理员', 'password': get_md5("1234qwer"), 'is_active': True},
+            email="super@super.com")
+        User2Role.objects.update_or_create(user=super_account, role=super_role)
 
-        if not Account.objects.filter(email="admin@admin.com").exists():
-            admin_account, _ = Account.objects.update_or_create(
-                defaults={'username': "admin", 'nickname': '管理员', 'password': get_md5("1234qwer"), 'is_active': True},
-                email="admin@admin.com")
-            User2Role.objects.update_or_create(user=admin_account, role=admin_role)
+        admin_account, _ = Account.objects.update_or_create(
+            defaults={'username': "admin", 'nickname': '管理员', 'password': get_md5("1234qwer"), 'is_active': True},
+            email="admin@admin.com")
+        User2Role.objects.update_or_create(user=admin_account, role=admin_role)
