008412c3597ee9243a60ecc0efd65a878f5efd02
Wang, Xiaochuan <wangxiaochuan01@163.com>
2024-05-09 15:54:51 +0800

    用户磁盘使用&任务使用数量为-1时没有限制

--- bioinformatics-analysis/docs/db/update.sql
+++ bioinformatics-analysis/docs/db/update.sql
@@ -2,6 +2,6 @@
 -- config增加字符串类型配置字段，方便保存各类配置
 alter table config add data varchar(1000);
 
--- 删除字段约束, 不需要>0的限制
+-- 2024.05.09 删除字段约束, 不需要>0的限制
 alter table account drop constraint account_disk_limit_check;
 alter table account drop constraint account_used_disk_check;
