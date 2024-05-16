454bae3ca751bfcd7962051bb300f11e3ad9a713
Mering <Mering>
2024-05-16 23:50:08 +0800

    fix

--- bioinformatics-analysis/patient/migrations/0009_alter_20240516_2300.py
+++ bioinformatics-analysis/patient/migrations/0009_alter_20240516_2300.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("patient", "00008_auto_20230501_1519"),
+        ("patient", "0008_auto_20230501_1519"),
     ]
 
     operations = [
