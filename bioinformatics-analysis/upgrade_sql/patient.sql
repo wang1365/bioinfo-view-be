1dc59e604efae68af76c0ccb43bf3856c90dcd75
mering <canhuayin@gmail.com>
2025-04-13 21:49:12 +0800

    修改表名称

--- bioinformatics-analysis/upgrade_sql/patient.sql
+++ bioinformatics-analysis/upgrade_sql/patient.sql
@@ -1,25 +1,25 @@
 --- 患者新增字段 2025 金域平台功能需求
 
-ALTER TABLE patient
+ALTER TABLE patient_patient
 ADD COLUMN general_pathology_number VARCHAR(256) DEFAULT '' NULL;
-COMMENT ON COLUMN patient.general_pathology_number IS '常规病理编号';
+COMMENT ON COLUMN patient_patient.general_pathology_number IS '常规病理编号';
 
-ALTER TABLE patient
+ALTER TABLE patient_patient
 ADD COLUMN molecular_pathology_number VARCHAR(256) DEFAULT '' NULL;
-COMMENT ON COLUMN patient.molecular_pathology_number IS '分子病理编号';
+COMMENT ON COLUMN patient_patient.molecular_pathology_number IS '分子病理编号';
 
-ALTER TABLE patient
+ALTER TABLE patient_patient
 ADD COLUMN submitting_department VARCHAR(256) DEFAULT '' NULL;
-COMMENT ON COLUMN patient.submitting_department IS '送检科室';
+COMMENT ON COLUMN patient_patient.submitting_department IS '送检科室';
 
-ALTER TABLE patient
+ALTER TABLE patient_patient
 ADD COLUMN patient_phone_number VARCHAR(256) DEFAULT '' NULL;
-COMMENT ON COLUMN patient.patient_phone_number IS '患者电话';
+COMMENT ON COLUMN patient_patient.patient_phone_number IS '患者电话';
 
-ALTER TABLE patient
+ALTER TABLE patient_patient
 ADD COLUMN outpatient_or_inpatient_number VARCHAR(256) DEFAULT '' NULL;
-COMMENT ON COLUMN patient.outpatient_or_inpatient_number IS '门诊/住院号';
+COMMENT ON COLUMN patient_patient.outpatient_or_inpatient_number IS '门诊/住院号';
 
-ALTER TABLE patient
+ALTER TABLE patient_patient
 ADD COLUMN bed_number VARCHAR(256) DEFAULT '' NULL;
-COMMENT ON COLUMN patient.bed_number IS '床号';
+COMMENT ON COLUMN patient_patient.bed_number IS '床号';
